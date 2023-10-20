74bc43ca05cf710421a149984ea97b294645bb30
benvallin <ben.vallin@gmail.com>
2023-10-20 17:08:36 +0100

    Initial commit
    Changes to be committed:
    	new file:   batch_fastqc.sh
    	new file:   batch_pe_salmon_quant.sh
    	new file:   batch_pe_trim_galore.sh
    	new file:   sample_to_barcode_directory.sh
    
    hello

---
+++ batch_fastqc.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+### Define help message
+HELP='
+NAME
+  batch_fastqc.sh - run fastqc on a set of samples
+
+SYNOPSIS
+  batch_fastqc.sh -s -o [-n -t]
+  batch_fastqc.sh -h
+
+DESCRIPTION
+  Run fastqc on a set of samples.
+  -s  File listing the samples to operate on.
+      Each line should contain the path to a sample-specific directory containing fastq files.
+      Path can be absolute or relative to the current directory.
+  -o  Path to output directory (passed as -o to fastqc).
+      Path can be absolute or relative to the current directory.
+  -n  Regexp matching the name of fastq files to parse. Default is \"*fastq.gz\".
+  -t  The number of threads to use concurrently (passed as -t to fastqc). Default is 8.
+  -h  Print this help.\n
+'
+
+### Parse option arguments
+while getopts ":hs:o:n:t:" opt
+do
+  case $opt in
+    h) printf "$HELP"
+    exit 0
+    ;;
+    s) SAMPLES="$OPTARG"
+    ;;
+    o) OUTDIR="${OPTARG%/}"
+    ;;
+    n) NAMES="$OPTARG"
+    ;;
+    t) THREADS="$OPTARG"
+    ;;
+    \?) printf "Invalid option -$OPTARG\n"
+    exit 1
+    ;;
+    :) printf "Option -$OPTARG requires a valid argument\n" 
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) printf "Option -$opt needs a valid argument\n"
+    exit 1
+    ;;
+  esac
+done
+
+# => Set names regexp to default value if not provided
+if ! [[ -v NAMES ]]
+then 
+  NAMES="*fastq.gz"
+fi
+
+# => Set n threads to default value if not provided
+if ! [[ -v THREADS ]]
+then 
+  THREADS=8
+fi
+
+### Record samples and fastq files details
+N_SAMPLES=$(cat "$SAMPLES" | wc -l)
+
+FQFILES=$(for i in $(cat "$SAMPLES"); do find "$i" -name "$NAMES"; done)
+
+N_FQFILES=$(echo "$FQFILES" | wc -l)
+
+printf "\n\n--->$N_FQFILES FASTQ FILES TO PROCESS OUT OF $N_SAMPLES SAMPLES<---\n\n"
+
+### Create output directory if it does not exist already
+if ! [[ -d "$OUTDIR" ]]
+then 
+  mkdir -p "$OUTDIR"
+fi
+
+### Run fastqc
+fastqc -o "$OUTDIR" -t "$THREADS" $(echo "$FQFILES")
+
+printf "\n\n--->ALL JOBS COMPLETE<---\n\n"
+
---
+++ sample_to_barcode_directory.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+### Define help message
+HELP='
+NAME
+  sample_to_barcode_directory.sh - move barcode-specific fastq files from sample-specific to barcode-specific directories  
+
+SYNOPSIS
+  sample_to_barcode_directory.sh -s -o [-p -d]
+  sample_to_barcode_directory.sh -h
+
+DESCRIPTION
+  Move barcode-specific fastq files stored in sample-specific directories to barcode-specific directories.
+  -s  File listing the samples to operate on.
+      Each line should contain the path to a sample-specific directory containing barcode-specific fastq files.
+      Path can be absolute or relative to the current directory. File names should contain the ID of the corresponding barcode. 
+  -o  Path to output directory. Can be absolute or relative to the current directory.
+      Files are moved to barcode-specific subdirectories within the provided directory.
+  -p  Regexp matching a string to remove from the file names in order to generate barcode-specific names. Default is \"_R[12].*$\".
+  -d  If provided, the input sample-specific directories are deleted once the job is complete.
+  -h  Print this help.\n
+'
+
+### Parse option arguments
+while getopts ":hs:o:p:d" opt
+do
+  case $opt in
+    h) printf "$HELP"
+    exit 0
+    ;;
+    s) SAMPLES="$OPTARG"
+    ;;
+    o) OUTDIR="${OPTARG%/}"
+    ;;
+    p) PATTERN="$OPTARG"
+    ;;
+    d) DELETE=
+    ;;
+    \?) printf "Invalid option -$OPTARG\n"
+    exit 1
+    ;;
+    :) printf "Option -$OPTARG requires a valid argument\n" 
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) printf "Option -$opt requires a valid argument\n"
+    exit 1
+    ;;
+  esac
+done
+
+# => Set pattern regexp to default value if not provided
+if ! [[ -v PATTERN ]]
+then 
+  PATTERN="_R[12].*$"
+fi
+
+### Record samples details
+N_SAMPLES=$(cat "$SAMPLES" | wc -l)
+
+printf "\n\n--->$N_SAMPLES SAMPLES TO PROCESS<---\n"
+
+### Move fastq files to barcode-specific directories
+for i in $(cat "$SAMPLES")
+do 
+  BARCODES=$(sed "s/$PATTERN//g" <(ls "$i"))
+  N_BARCODES=$(echo "$BARCODES" | wc -l)
+  printf "\n--->Sample $(basename $i): creating $N_BARCODES barcode-specific directories<---" 
+  for j in $(echo "$BARCODES")
+  do
+    if ! [[ -d "$OUTDIR"/"$j" ]]
+    then 
+      mkdir -p "$OUTDIR"/"$j"
+    fi
+    for k in $(find "$i" -name "$j*")
+    do 
+      mv "$k" "$OUTDIR"/"$j"
+    done
+  done
+done
+
+### Delete input sample-specific directories if requested
+if [[ -v DELETE ]]
+then
+  printf "\n\n--->Deleting input sample-specific directories...<---\n"
+  rm -r $(cat "$SAMPLES")
+fi
+
+printf "\n--->JOB COMPLETE<---\n\n"
